62d79554299d8c7ae68ff09bfb47bf3b9799c3fc
vkaja <tom1870699@gmail.com>
2022-11-08 13:49:35 +0800

    bug fixed:coolapk

--- QuantumultX/File/coolapk+.js
+++ QuantumultX/File/coolapk+.js
@@ -4,7 +4,7 @@ object['data'].forEach((item, index) => {
     if (index >= 2) {
         let msg = item['message'];
         console.log(msg);
-        if (msg.search('华为') || msg.search('鸿蒙') || msg.search('荣耀')) {
+        if (msg.indexOf('华为') || msg.indexOf('鸿蒙') || msg.indexOf('荣耀')) {
             delete object['data'][index];
             console.log('coolapk+:已过滤一条动态');
         }
